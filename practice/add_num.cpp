4235ae46aa145d7296b7e3abfa5f0fceb38525db
Kanshuman42 <ANSHUMANROY852@GMAIL.COM>
2022-03-17 14:47:57 +0530

    addnumbers

---
+++ practice/add_num.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long 
+#define mod 1000000007
+void solve()
+{int a = 5 ; 
+int b = 6 ; 
+cout<< a+b <<endl ; 
+    
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("C:\\Users\\Anshuman\\Desktop\\c++\\input.txt", "r", stdin);
+    freopen("C:\\Users\\Anshuman\\Desktop\\c++\\output.txt", "w", stdout);
+#endif
+
+    int test=1;
+//cin>>test;
+    while(test--)
+    {
+        solve();
+    }
+    return 0;
+}
